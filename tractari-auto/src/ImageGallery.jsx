097a5e421073c01550d396024b9bd5b54ede995d
alinamarina07 <137393068+alinamarina07@users.noreply.github.com>
2024-12-25 15:13:38 +0200

    app

---
+++ tractari-auto/src/ImageGallery.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import image1 from '../public/image1.jpg';
+import image2 from '../public/image2.jpg';
+import image3 from '../public/image3.jpg';
+import image4 from '../public/image4.jpg';
+import image5 from '../public/image5.jpg';
+import image6 from '../public/image6.jpg';
+import image7 from '../public/image7.jpg';
+import image8 from '../public/image8.jpg';
+import image9 from '../public/image9.jpg';
+import './ImageGallery.css';
+
+const ImageGallery = () => {
+  const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const closeLightbox = () => setSelectedImage(null);
+
+  return (
+    <div className='imageGallery'>
+      <h1>Galerie foto</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '10px' }}>
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Gallery ${index + 1}`}
+            style={{ width: '150px', height: '120px', cursor: 'pointer', margin: '9px' }}
+            onClick={() => setSelectedImage(image)}
+          />
+        ))}
+      </div>
+
+      {selectedImage && (
+        <div className='styleImages'
+        
+
+          onClick={closeLightbox}
+        >
+          <img
+            src={selectedImage}
+            alt="Large"
+            style={{ maxWidth: '90%', maxHeight: '90%' }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageGallery;
